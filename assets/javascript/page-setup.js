a7815119d3dea1889938febce943c854a4f5463d
NiBeCa <nbcald@gmail.com>
2019-03-25 13:01:24 -0500

    added firebase input field and datepicker

--- assets/javascript/page-setup.js
+++ assets/javascript/page-setup.js
@@ -1,30 +1,31 @@
 $(document).ready(function () {
 
+
 function createNavbar(){
 
-  var navbarContent = $('<div class="nav-wrapper container">');
-    navbarContent.append($(' <a id="logo-container" href="index.html" class="brand-logo"><img src="assets/images/logo-fav.png"></a>'));
-    navbarContent.append($('<a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>'));
+  var navbarContent = $('<div class="nav-wrapper container"></div>');
+    navbarContent.append($('<a id="logo-container" href="index.html" class="brand-logo"><img src="assets/images/logo-fav.png"></a>'));
+    navbarContent.append($('<a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>'));
 
-  var navbarMenu = $('<ul class="right hide-on-med-and-down menu-text">');
+  var navbarMenu = $('<ul class="right hide-on-med-and-down menu-text"></ul>');
     navbarMenu.append($('<li><a href="index.html">Home</a></li>'));
     navbarMenu.append($('<li><a href="events.html">Events</a></li>'));
     navbarMenu.append($('<li><a href="movies.html">Movies</a></li>'));
     navbarContent.append(navbarMenu);
 
-  var navbarMobileMenu = $('<ul id="nav-mobile" class="sidenav">');
+  var navbarMobileMenu = $('<ul class="sidenav" id="mobile-demo"></ul>');
     navbarMobileMenu.append($('<li><a href="index.html">Home</a></li>'));
     navbarMobileMenu.append($('<li><a href="events.html">Events</a></li>'));
     navbarMobileMenu.append($('<li><a href="movies.html">Movies</a></li>'));
     navbarContent.append(navbarMobileMenu);
-
+   
   $(".navigation").append(navbarContent);
 }
 createNavbar();
 
 function createFooter(){
-  var rowIcon = $('<div class="row row-icons">');
-  var socialMediaIcons = $('<ul class="icons">');
+  var rowIcon = $('<div class="row row-icons"></div>');
+  var socialMediaIcons = $('<ul class="icons"></ul>');
   socialMediaIcons.append($('<li><a class="sm-icon" href="assets/images/Facebook.png"><img src="assets/images/facebook-logo.png"></a></li>'));
   socialMediaIcons.append($('<li><a class="sm-icon" href="assets/images/Twitter.png"><img src="assets/images/twitter-logo.png"></li>'));
   socialMediaIcons.append($('<li><a class="sm-icon" href="assets/images/Instagram.png"><img src="assets/images/instagram-logo.png"></a></li>'));
@@ -32,15 +33,15 @@ function createFooter(){
   rowIcon.append(socialMediaIcons);
   $(".page-footer").append(rowIcon);
 
-  var rowLinks = $('<div class="row row-links">');
-  var pageLinks = $('<ul class="links">');
+  var rowLinks = $('<div class="row row-links"></div>');
+  var pageLinks = $('<ul class="links"></ul>');
   pageLinks.append($('<li><a class="black-text link-position" href="about.html">About Us</a></li>'));
   pageLinks.append($('<li><a class="black-text link-position" href="contact.html">Contact Us</a></li>'));
   rowLinks.append(pageLinks);
   $(".page-footer").append(rowLinks);
 
-  var rowCopyright = $('<div class="footer-copyright black">');
-  var copyrightContainer = $('<div class="container">');
+  var rowCopyright = $('<div class="footer-copyright black"></div>');
+  var copyrightContainer = $('<div class="container"></div>');
   copyrightContainer.append($('<a class="white-text">© Local Event Spotter 2019. All rights reserved.</a>'));
   rowCopyright.append(copyrightContainer);
   $(".page-footer").append(rowCopyright);
@@ -49,18 +50,18 @@ createFooter();
 
 function createTextBanner(){
 
-  var bannerLogo= $('<div class="banner-logo center">');
+  var bannerLogo= $('<div class="banner-logo center"></div>');
   bannerLogo.append($('<a href="index.html"><img src="assets/images/logo-6.png"></a>'));
   $(".row-1").append(bannerLogo);
 
-  var zipCodeInput= $('<form class=" zip-code-input col s10 offset-s3">');
+  var zipCodeInput= $('<form class=" zip-code-input col s12"></form>');
 
-  var zipCodeField= $('<div class="input-field ">');
-  zipCodeField.append($('<input class id="zip" name= "zip" type="text" required pattern="[0-9]{5}">'));
+  var zipCodeField= $('<div class="input-field col s2"></div>');
+  zipCodeField.append($('<input class id="zip" name= "zip" type="text" required pattern="[0-9]{5}"></input>'));
   zipCodeField.append($('<label class="label-icon grey-text" for="search">Enter a zip code</label>'));
   zipCodeInput.append(zipCodeField);
 
-  var zipCodeMilesSelect= $('<select class="browser-default">');
+  var zipCodeMilesSelect= $('<select class="browser-default" col s2></select>');
   zipCodeMilesSelect.append($('<option value="" disabled selected>Mile from Zip Code</option>'));
   zipCodeMilesSelect.append($('<option value="1">5 Miles</option>'));
   zipCodeMilesSelect.append($('<option value="2">10 Miles</option>'));
@@ -71,12 +72,14 @@ function createTextBanner(){
   var zipCodeMilesLabel= $('<label class="label-icon grey-text" for="search"></label>');
   zipCodeInput.append(zipCodeMilesLabel);
 
+  var datePicker= $('<div class="date-field col s4"></div>');
+  datePicker.append($('<input type= "date" class= "datepicker"></input>'));
+  zipCodeInput.append(datePicker);
+
  $(".row-3").append(zipCodeInput);
 }
+
 createTextBanner();
 
-
+  
 });
-
-
-
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,38 +1,20 @@
 $(document).ready(function () {
-
   //----- Materialize features ---------------------//
   $('.sidenav').sidenav();
   $('.slider').slider();
   $('.materialboxed').materialbox();
-});
+  $('textarea#textarea').characterCounter();
+
+
+var cl = console.log;
 
 //  --- AJAX for Seatgeek --------------------------------------------------------------//
-// -------------------- 
-var cl = console.log;
-// Add an event listener to Zip Code variable, limit it to mile range
-var zipCode =  75211;
-//Add an event listener for the drop-down radius of the miles
-var mileRange= "10mi";
-var queryURL = "https://api.seatgeek.com/2/events?geoip=" + zipCode + "&range=" + mileRange + "&client_id=MTU4NDc5NTh8MTU1MzEzMDYzNy4zNA";
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function (response) {
-      cl(queryURL);
-      cl(response);
-      cl("Events: " + response.events);
-  });
-
-
-
-
-// //  --- AJAX for Seatgeek --------------------------------------------------------------//
-// // -------------------- 
-// var cl = console.log
-// // var zipCode = $(this).attr("data-name");
-// var zipCode = 75211
-// var queryURL = "https://api.seatgeek.com/2/venues?postal_code=" + zipCode + "&client_id=MTU4NDc5NTh8MTU1MzEzMDYzNy4zNA";
+// // Add an event listener to Zip Code variable, limit it to mile range
+// var zipCode =  75211;
+// //Add an event listener for the drop-down radius of the miles
+// var mileRange= "10mi";
+// var queryURL = "https://api.seatgeek.com/2/events?geoip=" + zipCode + "&range=" + mileRange + "&client_id=MTU4NDc5NTh8MTU1MzEzMDYzNy4zNA";
 
 // $.ajax({
 //     url: queryURL,
@@ -40,22 +22,63 @@ $.ajax({
 //   }).then(function (response) {
 //       cl(queryURL);
 //       cl(response);
+//       cl("Events: " + response.events);
+//   });
+
+
+//  --- Firebase for contact form------------------------------------------------------//
+
+  var config = {
+    apiKey: "AIzaSyB57NQ2m4VxhU4-b6LORPi4MPDeCC5POMU",
+    authDomain: "smu-p1-g5.firebaseapp.com",
+    databaseURL: "https://smu-p1-g5.firebaseio.com",
+    projectId: "smu-p1-g5",
+    storageBucket: "smu-p1-g5.appspot.com",
+    messagingSenderId: "1039960867810"
+  };
+
+  firebase.initializeApp(config);
+  var fdb = firebase.database();
+
+  var firstName = "";
+  var lastName = "";
+  var email = "";
+  var message = "";
+
+  $("#contact-form-submit").on("click", function(event) {
+    event.preventDefault();
+    formInput();
+  });
+
+
+  function formInput(){
+
+    firstName = $("#input-first-name").val().trim();
+    lastName = $("#input-last-name").val().trim();
+    email = $("#input-email").val().trim();
+    message = $("#input-message").val().trim();
+    cl(firstName);
+
+    fdb.ref().push({
+      firstName : firstName,
+      lastName : lastName,
+      email : email,
+      message : message,
+      });
+  }
+
+  fdb.ref().on("child_added", function(childSnapshot) {
+    var fsv = childSnapshot.val();
+
+    var contactResponseText = (fsv.firstName + ", thank you for contacting us. You will hear from us soon.");
+    $(".contact-info-text").append(contactResponseText);
+  },
+    function(errorObject) {
+      cl("The read failed: " + errorObject.code);
+
+    }); 
+});
 
 
 
 
-
-
-
-
-
-      // //-- Initialize Firebase ----------------------------------------------//
-      // var config = {
-      //   apiKey: "AIzaSyAyBYJFRwwDHbOGYKG9CZkqtAinxmP6nDo",
-      //   authDomain: "smu-g5-p1-eventpage.firebaseapp.com",
-      //   databaseURL: "https://smu-g5-p1-eventpage.firebaseio.com",
-      //   projectId: "smu-g5-p1-eventpage",
-      //   storageBucket: "smu-g5-p1-eventpage.appspot.com",
-      //   messagingSenderId: "48500713462"
-      // };
-      // firebase.initializeApp(config);
